c4ba404d5b7087d18b0112c01137acd0a064068d
Sourabh Shrimali <shrimali.sourabh@yahoo.com>
2024-06-22 17:06:35 +0530

    finish caluclater app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,36 @@
+import { useState } from "react";
 import Header from "./components/Header"
+import UserInput from "./components/UserInput"
+import Result from "./components/Result";
 
 function App() {
+
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 120000,
+    expectedReturn: 6,
+    duration: 10
+});
+
+function handleChange(event) {
+    setUserInput(previous => {
+        const value = event.target.value
+        return {
+            ...previous,
+            [event.target.name]: +value
+        }
+    })
+}
+ 
+const isValidInput = userInput.duration >= 1;
+
   return (
-    <Header/>
+    <>
+      <Header/>
+      <UserInput userInput={userInput} handleChange={handleChange}/>
+      { !isValidInput && <p className="center">Please enter duration greater than zero.</p>}
+      { isValidInput && <Result input={userInput}/>}
+    </>
   )
 }
 
--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,12 +1,16 @@
-export default function UserInput() {
+import { useState } from "react"
+
+export default function UserInput({ userInput, handleChange}) {
+
+
     return <section id="user-input">
         <div className="input-group">
-            <p><label>Initial Investment</label><input type="number"/></p>
-            <p><label>Annual Investment</label><input type="number"/></p>
+            <p><label>Initial Investment</label><input type="number" value={userInput.initialInvestment} name="initialInvestment" onChange={handleChange} /></p>
+            <p><label>Annual Investment</label><input type="number" value={userInput.annualInvestment} name="annualInvestment" onChange={handleChange} /></p>
         </div>
         <div className="input-group">
-            <p><label>Expected Return</label><input type="number"/></p>
-            <p><label>Duration </label><input type="number"/></p>
+            <p><label>Expected Return</label><input type="number" value={userInput.expectedReturn} name="expectedReturn" onChange={handleChange} /></p>
+            <p><label>Duration </label><input type="number" value={userInput.duration} name="duration" onChange={handleChange} /></p>
         </div>
     </section>
 }
